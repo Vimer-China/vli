e06e2b58a263ddaa736e453523ee0131bbdd716e
calidion <calidion@gmail.com>
2017-03-04 21:24:56 +0800

    fixed some bug on ubuntu and general

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 if [ -d ~/.vli ]; then
   mkdir ~/.vli
 fi
+git clone https://github.com/Vimer-China/vli.git ~/.vli
 rm -rf ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cd ~/.vli
 cp -rf vimrc ~/.vimrc
 vim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe
--- ubuntu.init.sh
+++ ubuntu.init.sh
@@ -1,2 +1,2 @@
-sudo apt-get install python-dev python3-dev
-sudo apt-get install build-essential cmake
+sudo apt-get install -y python-dev python3-dev
+sudo apt-get install -y build-essential cmake
