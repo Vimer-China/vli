929b9424fe9da696fe722157595f5ef7d67b2557
Eric <calidion@users.noreply.github.com>
2017-03-17 20:58:37 +0800

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -1,2 +1,4 @@
-sudo apt-get install python-dev python3-dev
-sudo apt-get install build-essential cmake
+brew install cmake
+brew install go
+brew unlink python # 解决安装完成后出现的提示bug，如果你的系统没有碰到问题，可以不用执行
+brew install mono
